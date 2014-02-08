8cbb7e13ed51f2cbadfeb5d64009c9604d86465d
brendajin <brendajin@gmail.com>
2014-02-08 14:43:34 -0800

    added basic hello world message on port 1337

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-var express = require('express');
-var app = express();
-
-
-app.configure(function(){
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
-
-
-app.listen(3000);
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
