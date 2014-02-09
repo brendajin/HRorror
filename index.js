f1a3ce8ded2af8c78048dba34ba0860e283c7f02
brendajin <brendajin@gmail.com>
2014-02-08 16:23:56 -0800

    added static file server to project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   var body = 'Hello World';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', Buffer.byteLength(body));
   res.send(body);
 });
+
 
 app.listen(3001);
