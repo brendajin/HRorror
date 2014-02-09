6a38803e10839a8f0a5b20973030757c4fb326fc
brendajin <brendajin@gmail.com>
2014-02-09 01:37:44 -0800

    added logic that removes the data for the posts from local memory onthe page

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,16 @@
-var url = 'https://burning-fire-1978.firebaseio.com/'
-
 // Get a reference to the root of the chat data.
-var messagesRef = new Firebase(url + 'messages');
-
-var renderedStickies = [];
+var url = 'https://burning-fire-1978.firebaseio.com/',
+    messagesRef = new Firebase(url + 'messages'),
+    renderedStickies = []; //references to all the stickies that have been rendered
 
 // When the user presses enter on the message input, write the message to firebase.
 $('#messageInput').keypress(function (e) {
-  var myMessages = messagesRef.push()
-
+  var myMessages = messagesRef.push(),
+      name = $('#nameInput').val(),
+      text = $('#messageInput').val(),
+      timeNow = (new Date().getTime());
 
   if (e.keyCode == 13) {
-    var name = $('#nameInput').val(),
-    		text = $('#messageInput').val(),
-    		timeNow = (new Date().getTime());
     myMessages.set({name:name, text:text, time:timeNow});
     $('#messageInput').val('');
   }
@@ -22,13 +19,13 @@ $('#messageInput').keypress(function (e) {
 // render the messages
 var render = function (snapshot) {
   var message = snapshot.val();
+  // if a sticky gets rendered, push some data into the renderedStickies array to keep track of it
   renderedStickies.push({loc: snapshot.name(), time: message.time});
 
   $('<div id=' + message.time + ' class="sticky"/>').text(message.text).prepend($('<span/>')
     .text(message.name+': ')).appendTo($('#messagesDiv'));
   $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
 }
-
 
 //create a timeout function that counts down from 30 seconds, then removes the entry from the server at expiration
 var myTimeout = function() {
@@ -39,12 +36,14 @@ var myTimeout = function() {
   for(var i = 0; i < renderedStickies.length; i++) {
     if(myTime - renderedStickies[i].time > 30*1000) {
       myExpiredRefs.push(renderedStickies[i]);
+      renderedStickies.splice(i, 1); //remove the element from the rendered array
     }
   }
 
   for(var i = 0; i < myExpiredRefs.length; i++) {
     myRef = new Firebase(url + 'messages/' + myExpiredRefs[i].loc);
     myRef.remove();
+    myExpiredRefs.splice(i, 1); //remove the element from the expired array
   }
 }
 
