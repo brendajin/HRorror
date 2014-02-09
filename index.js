e0fee29c3198dc48421941c2fd5e22d7a85c71e6
brendajin <brendajin@gmail.com>
2014-02-09 12:46:20 -0800

    made some changes to index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ timeout.init();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', Buffer.byteLength(body));
-  res.send(body);
-});
+// app.get('/', function(req, res){
+//   var body = 'Hello World';
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.setHeader('Content-Length', Buffer.byteLength(body));
+//   res.send(body);
+// });
 
 
 app.listen(3001);
