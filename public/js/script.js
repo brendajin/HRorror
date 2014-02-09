a1e68f63a808a4f16e965d9f9945a3d527a01911
brendajin <brendajin@gmail.com>
2014-02-09 13:57:49 -0800

    added twitter button script to script.js

--- public/js/script.js
+++ public/js/script.js
@@ -62,3 +62,9 @@ messagesRef.on('child_removed', function(snapshot) {
   
 })
 setInterval(myTimeout, 1000)
+
+$(function(){
+  $("#messageInput").characterCounter(); 
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+});
+
